4b873aa0dbe7117dfcf9123eef26de291c7666cf
Vaibhav Trikolikar <vaibhavtrikolikar412@gmail.com>
2024-06-14 23:48:12 +0530

    Wizard Coontrol Code Added

---
+++ Common/WizardControl/ASP_Demo/WebForm.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP_Demo
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        string CS = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Wizard1_NextButtonClick(object sender, WizardNavigationEventArgs e)
+        {
+            if (e.NextStepIndex == 2)
+            {
+                lblFirstName.Text = txtFirstName.Text;
+                lblLastName.Text = txtLastName.Text;
+                lblGender.Text = ddlGender.SelectedValue;
+
+                lblEmail.Text = txtEmail.Text;
+                lblMobile.Text = txtMobile.Text;
+            }
+        }
+
+        protected void Wizard1_FinishButtonClick(object sender, WizardNavigationEventArgs e)
+        {
+            Response.Redirect("~/Confirmation.aspx");
+        }
+    }
+}
